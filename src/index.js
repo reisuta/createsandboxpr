be0976b678ca4201d1e0cfdf45cfd491744d26da
amaki <79194040+reisuta@users.noreply.github.com>
2021-06-27 17:13:09 +0900

    pre todo comp

--- src/index.js
+++ src/index.js
@@ -7,4 +7,22 @@ add.addEventListener("click", () => {
   li.textContent = in1.value;
   list.appendChild(li);
   in1.value = "";
+
+  const button = document.createElement("button");
+  button.classList.add("remove");
+  button.textContent = "削除";
+  li.appendChild(button);
+
+  const button2 = document.createElement("button");
+  button2.classList.add("solve");
+  button2.textContent = "終了";
+  li.appendChild(button2);
+
+  button.addEventListener("click", () => {
+    list.removeChild(li);
+  });
+
+  button2.addEventListener("click", () => {
+    li.classList.add("delete");
+  });
 });
